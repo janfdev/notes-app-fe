a191882d66ee5502d691b18b3a54e3181d7d238d
rizqi-noor-fauzan <fauzanrizqinoor@gmail.com>
2025-05-01 22:10:50 +0700

    Remove unnecessary whitespace in Navbar component for cleaner code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,6 @@ const Navbar = () => {
   return (
     <nav className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-
       <ProfileInfo name="john doe" onLogout={handleLogout} />
     </nav>
   );
