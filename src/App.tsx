6ae0349c203fa2a24ce903d8e13a8333bd5244dc
rizqi-noor-fauzan <fauzanrizqinoor@gmail.com>
2025-05-03 21:25:49 +0700

    Add DefaultPage component to routing: set as the landing page at '/'

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Home from "./pages/Home/Home";
 import { Route, Routes } from "react-router";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
+import DefaultPage from "./pages/default";
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<DefaultPage />} />
       <Route path="/dashboard" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
