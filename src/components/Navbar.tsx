e9a71ff29e5fa4ff77ec4cbdd17c0c4acd336df6
rizqi-noor-fauzan <fauzanrizqinoor@gmail.com>
2025-05-03 22:33:32 +0700

    Integrate search functionality into Navbar: add state management for search query and connect SearchBar component with search and clear handlers

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,33 @@
 import { useNavigate } from "react-router";
+import { useState } from "react";
 import ProfileInfo from "./Cards/ProfileInfo";
 import SearchBar from "./SearchBar/SearchBar";
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
 
   const handleLogout = () => {
     navigate("/login");
   };
+
+  const handleSearch = () => {};
+
+  const handleClearSearch = () => {
+    setSearchQuery("");
+  };
   return (
     <nav className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-      <SearchBar />
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={handleClearSearch}
+      />
       <ProfileInfo name="john doe" onLogout={handleLogout} />
     </nav>
   );
