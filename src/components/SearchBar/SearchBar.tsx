97e6ef8916e35b54c9f4d5f36ad1488c6ab03bef
rizqi-noor-fauzan <fauzanrizqinoor@gmail.com>
2025-05-03 21:42:41 +0700

    Refactor SearchBar component: enhance styling, add search and clear functionality with icons

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
+
 type SearchBarProps = {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -5,10 +9,34 @@ type SearchBarProps = {
   onClearSearch: () => void;
 };
 
-const SearchBar = ({ value, onChange, handleSearch, onClearSearch }) => {
-  return <div>
-    <input type="text" />
-  </div>;
+const SearchBar = ({
+  value,
+  onChange,
+  handleSearch,
+  onClearSearch,
+}: SearchBarProps) => {
+  return (
+    <div className="w-80 flex items-center px-4 bg-slate-100 rounded-md">
+      <input
+        type="text"
+        placeholder="Search Notes"
+        className="w-full text-xs bg-transparent py-[11px] outline-none"
+        value={value}
+        onChange={onChange}
+      />
+
+      {value && (
+        <IoMdClose
+          onClick={onClearSearch}
+          className="text-xl text-slate-500 cursor-pointer hover:text-black mr-3"
+        />
+      )}
+      <FaMagnifyingGlass
+        className="text-gray-400 cursor-pointer hover:text-black"
+        onClick={handleSearch}
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
