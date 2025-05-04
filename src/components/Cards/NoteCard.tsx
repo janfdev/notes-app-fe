8f7417d6e82eb2108d79074bc00d1f2235f9f5c7
rizqi-noor-fauzan <fauzanrizqinoor@gmail.com>
2025-05-04 19:13:37 +0700

    Refactor NoteCard component: replace pin icon with new icon, enhance layout and styling for better user experience; remove unused CSS class

--- src/components/Cards/NoteCard.tsx
+++ src/components/Cards/NoteCard.tsx
@@ -1,4 +1,4 @@
-import { MdOutlinePushPin } from "react-icons/md";
+import { BsPinAngleFill } from "react-icons/bs";
 import { MdCreate, MdDelete } from "react-icons/md";
 
 type NoteCardProps = {
@@ -24,23 +24,33 @@ const NoteCard = ({
 }: NoteCardProps) => {
   return (
     <section className="border rounded p-4  bg-white hover:shadow-xl w-md transition-all ease-in-out">
-      <div className="flex items-center justify-between">
-        <h3 className="text-medium text-lg capitalize">{title}</h3>
-        <span className="text-xs text-gray-500">{date}</span>
-      </div>
-      <div className="flex items-center justify-end mt-2">
-        <MdOutlinePushPin
-          className={`icon-btn ${isPinned ? "text-blue-500" : "text-black"}`}
+      <div
+        className="flex items-center justify-between
+      "
+      >
+        <div>
+          <h3 className="text-medium text-lg capitalize">{title}</h3>
+          <span className="text-xs text-gray-500">{date}</span>
+        </div>
+        <div
+          className={`text-xl text-slate-600 cursor-pointer hover:text-blue-700 hover:border-blue-700 rounded-full border border-slate-600 w-10 h-10 flex items-center justify-center ${
+            isPinned ? "text-blue-500 border-blue-500" : "text-black"
+          }`}
           onClick={onPinNote}
-        />
+        >
+          <BsPinAngleFill />
+        </div>
       </div>
-      <p className="text-wrap capitalize">{content?.slice(0, 60)}</p>
-      <div>
+
+      <p className="text-wrap capitalize text-xs text-slate-600 mt-2">
+        {content?.slice(0, 60)}
+      </p>
+      <div className="flex items-center justify-between">
         <div className="text-xs text-slate-100 rounded-full bg-slate-800">
           {tags}
         </div>
 
-        <div className="flex items-center justify-between gap-2">
+        <div className="flex items-center gap-2">
           <MdCreate
             className="text-2xl bg-green-500 hover:bg-green-700 text-white rounded-full p-1"
             onClick={onEdit}
