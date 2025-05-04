cf669825b8c8ff87c7a0de2df066045c824a519f
rizqi-noor-fauzan <fauzanrizqinoor@gmail.com>
2025-05-04 21:54:48 +0700

    Add input fields for title and content in AddEditNotes component: introduce new input-label class for consistent styling in the form.

--- src/pages/Home/AddEditNotes.tsx
+++ src/pages/Home/AddEditNotes.tsx
@@ -1,7 +1,25 @@
-import React from "react";
-
 const AddEditNotes = () => {
-  return <div>Edit Notes</div>;
+  return (
+    <section className="w-md">
+      <div className="flex flex-col gap-2">
+        <label className="input-label">TITLE</label>
+        <input
+          type="text"
+          className="text-2xl text-slate-950 outline-none placeholder:text-lg"
+          placeholder="Go to Gym at 5 PM"
+        />
+      </div>
+
+      <div className="flex flex-col mt-2 gap-2">
+        <label className="input-label">CONTENT</label>
+        <textarea
+          className="text-sm text-slate-950 outline-none border bg-slate-50 p-2 rounded"
+          placeholder="Content"
+          rows={10}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default AddEditNotes;
