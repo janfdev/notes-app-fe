f5a9d979953a6db2c22a20ab423b9423d9cf7fe6
rizqi-noor-fauzan <fauzanrizqinoor@gmail.com>
2025-05-05 22:24:43 +0700

    Enhance TagInput component by adding keyboard support for tag addition on Enter key press and improving tag display with a close button for removal.

--- src/components/Input/TagInput.tsx
+++ src/components/Input/TagInput.tsx
@@ -19,8 +19,26 @@ const TagInput = ({ tags, setTags }: TagInputProps) => {
       setInputValue("");
     }
   };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      addNewTag();
+    }
+  };
   return (
     <div>
+      {tags?.length > 0 && (
+        <div className="flex items-center gap-2">
+          {tags.map((tag, index) => (
+            <span key={index} className="">
+              # {tag}
+              <button onClick={() => {}}>
+                <MdClose />
+              </button>
+            </span>
+          ))}
+        </div>
+      )}
       <div className="flex items-center gap-4 mt-3">
         <input
           type="text"
@@ -28,9 +46,15 @@ const TagInput = ({ tags, setTags }: TagInputProps) => {
           onChange={handleInputChange}
           className="text-sm px-3 py-2 bg-transparent border rounded outline-none"
           placeholder="Add tags"
+          onKeyDown={handleKeyDown}
         />
 
-        <button className="w-8 h-8 flex items-center justify-center rounded  border border-blue-700 hover:bg-blue-700">
+        <button
+          onClick={() => {
+            addNewTag();
+          }}
+          className="w-8 h-8 flex items-center justify-center rounded  border border-blue-700 hover:bg-blue-700"
+        >
           <MdAdd className="text-2xl text-blue-700 hover:text-white" />
         </button>
       </div>
