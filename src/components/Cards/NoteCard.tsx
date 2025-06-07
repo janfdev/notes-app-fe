0579a81e19cf4311f515286717751958b2cbd5d5
janfdev <fauzanrizqinoor@gmail.com>
2025-06-08 01:04:11 +0700

    feat: update dependencies, enhance UI components, and add utility functions

--- src/components/Cards/NoteCard.tsx
+++ src/components/Cards/NoteCard.tsx
@@ -1,6 +1,15 @@
 import { BsPinAngleFill } from "react-icons/bs";
 import { MdCreate, MdDelete } from "react-icons/md";
 import moment from "moment";
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "../ui/card";
 
 type NoteCardProps = {
   title: string;
@@ -24,18 +33,21 @@ const NoteCard = ({
   onPinNote
 }: NoteCardProps) => {
   return (
-    <section className="border rounded-md p-4 bg-white hover:shadow-2xl transition-all ease-in-out">
-      <div
+    <Card className="border rounded-md p-4 bg-white hover:shadow-2xl transition-all ease-in-out">
+      <CardHeader
         className="flex items-center justify-between
       "
       >
         <div>
-          <h3 className="text-medium text-lg capitalize">{title}</h3>
-          <span className="text-xs text-gray-500">
+          <CardTitle className="font-poppins text-lg capitalize">
+            {title}
+          </CardTitle>
+          <CardDescription className="text-xs text-gray-500">
             {moment(date).format("DD MMMM YYYY")}
-          </span>
+          </CardDescription>
         </div>
-        <div
+        <Button
+          variant={"secondary"}
           className={`text-xl text-slate-600 cursor-pointer hover:text-blue-700 hover:border-blue-700 rounded-full border border-slate-600 w-10 h-10 flex items-center justify-center ${
             isPinned
               ? "text-blue-500 border-blue-500 bg-blue-200"
@@ -43,37 +55,33 @@ const NoteCard = ({
           }`}
           onClick={onPinNote}
         >
-          <BsPinAngleFill />
-        </div>
-      </div>
+          <BsPinAngleFill className="text-lg" />
+        </Button>
+      </CardHeader>
 
-      <p className="text-wrap capitalize text-xs text-slate-600 mt-2">
+      <CardContent className="text-wrap capitalize text-xs text-slate-600 mt-2">
         {content?.slice(0, 60)}
-      </p>
-      <div className="flex items-center justify-between mt-2">
+      </CardContent>
+      <CardFooter className="flex items-center justify-between mt-2">
         <div className="flex gap-2 flex-wrap">
           {tags.map((tag, index) => (
-            <span
-              key={index}
-              className="text-xs text-slate-100 px-2 py-1 rounded-full bg-slate-800"
-            >
+            <Button variant={"outline"} key={index}>
               #{tag}
-            </span>
+            </Button>
           ))}
         </div>
 
         <div className="flex items-center gap-2">
-          <MdCreate
-            className="text-2xl bg-green-500 hover:bg-green-700 text-white rounded-full p-1"
-            onClick={onEdit}
-          />
-          <MdDelete
-            className="text-2xl bg-red-500 hover:bg-red-700 text-white rounded-full p-1"
-            onClick={onDelete}
-          />
+          <Button variant={"default"} onClick={onEdit}>
+            <MdCreate />
+          </Button>
+
+          <Button variant={"destructive"} onClick={onDelete}>
+            <MdDelete />
+          </Button>
         </div>
-      </div>
-    </section>
+      </CardFooter>
+    </Card>
   );
 };
 
